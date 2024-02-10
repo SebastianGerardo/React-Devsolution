8e1232f0dbb3e2ae23e92ed8dad8aa0b2e32358a
Sebastián Gerardo <sebastiangerardo27@gmail.com>
2024-02-09 21:46:32 -0500

    filter flags

--- rest-countries-app/src/App.jsx
+++ rest-countries-app/src/App.jsx
@@ -4,26 +4,58 @@ import './App.css'
 
 function App() {
   const [data, setData] = useState([])
+  const [filter, setFilter] = useState("")
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
+    setLoading(true)
     const getData = async () => {
       const fetchData = await fetch("https://restcountries.com/v3.1/all")
       const response = await fetchData.json()
 
       setData(response)
+      setLoading(false)
     }
 
     getData()
   }, [])
 
-  console.log(data[0])
+  if (loading === true) {
+    return (
+      <div className='countries'>
+        <h1>Cargando...</h1>
+      </div>
+    )
+  }
+
+  const handleChange = (event) => {
+    setFilter(event.target.value)
+  }
 
   return (
-    <div className='countries'>
-      {data && data.length > 0 && data.map((country, index) => (
-        <Card key={index} data={country} />
-      ))}
-    </div>
+    <section>
+      <div className='searching'>
+        <input type="text" placeholder='Buscar...' /> 
+        <select value={filter} onChange={handleChange}>
+          <option value=""></option>
+          <option value="Africa">Africa</option>
+          <option value="Europe">Europa</option>
+          <option value="Antarctic">Antártida</option>
+          <option value="Americas">America</option>
+          <option value="Asia">Asia</option>
+        </select>
+      </div>
+      <div className='countries'>
+        {data && 
+         data.length > 0 && 
+         data
+         .filter(country => filter == "" || country.region === filter)
+         .map((country, index) => (
+            <Card key={index} data={country} />
+          )
+        )}
+      </div>
+    </section>
   )
 }
 
