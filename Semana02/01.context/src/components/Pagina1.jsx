3297c45f97316399ea5c6068be695b1ed8080446
Sebastián Gerardo <sebastiangerardo27@gmail.com>
2024-02-12 22:28:54 -0500

    formulario en pagina1

--- Semana02/01.context/src/components/Pagina1.jsx
+++ Semana02/01.context/src/components/Pagina1.jsx
@@ -1,5 +1,4 @@
-import { useContext } from "react"
-import { UserContext } from "../context/UserContext"
+import { useState } from "react"
 
 const dataPagina1 = {
   nombres: "Sebastian",
@@ -7,11 +6,23 @@ const dataPagina1 = {
 }
 
 export default function Pagina1 () {
-  const { data } = useContext(UserContext)
+  const [dataForm, setDataForm] = useState({})
+
+  const handleChange = (event) => {
+    setDataForm(prevData => ({ 
+      ...prevData,
+      [event.target.name]: event.target.value
+    }))
+  }
+
   return (
     <div>
+      <form action="">
+        <input type="text" name="nombre" value={dataForm.nombre} onChange={handleChange} />
+        <input type="text" name="apellido" value={dataForm.apellido} onChange={handleChange} />
+      </form>
       <pre>
-        {JSON.stringify(data || dataPagina1, null, 2)}
+        {JSON.stringify(dataForm, null, 2)}
       </pre>
     </div>
   )
